932ffda7c83255950b4af82f2016546d7d5303b3
Ashton Wong <ashton@ucsb.edu>
2024-07-05 11:10:49 -0500

    scrolling

--- scripts.js
+++ scripts.js
@@ -6,3 +6,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+document.querySelectorAll('.indicator').forEach(indicator => {
+    indicator.addEventListener('click', function () {
+        document.querySelector(this.getAttribute('data-target')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const indicators = document.querySelectorAll('.indicator');
+    let currentSection = '';
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - section.clientHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    indicators.forEach((indicator) => {
+        indicator.classList.remove('active');
+        if (indicator.getAttribute('data-target') === `#${currentSection}`) {
+            indicator.classList.add('active');
+        }
+    });
+});
